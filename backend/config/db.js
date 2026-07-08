97cd72d0ab71a29c2e6e2fdacdc4b661481c239a
Ali Husnain <alihusnain00019@gmail.com>
2026-07-08 21:51:18 +0500

    url changing

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/portfolio");
+    const conn = await mongoose.connect(process.env.MONGO_URI || "mongodb+srv://alihusnain00019_db_user:ali123456@cluster0.idth75o.mongodb.net/?appName=Cluster0");
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
