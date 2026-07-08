c42c3e87e9c6d4317dc67e93184f2d75812e15d0
Ali Husnain <alihusnain00019@gmail.com>
2026-07-08 19:45:44 +0500

    change frontend url

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "https://portfolio-qbi3.vercel.app",
   credentials: true,
 }));
 app.use(express.json());
